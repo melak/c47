4f43fbb08a1c7ed2a2af832b1ad9e62c3dded695
MihailJP <mihailjp@gmail.com>
2022-01-14 11:06:06 +0900

    Fix uppercase/lowercase switch in AIM

--- src/wp43s/keyboard.c
+++ src/wp43s/keyboard.c
@@ -1957,7 +1957,7 @@ void fnKeyUp(uint16_t unusedButMandatoryParameter) {
         if(currentSoftmenuScrolls()) {
           menuUp();
         }
-        else if((calcMode == CM_NORMAL || calcMode == CM_AIM || calcMode == CM_NIM) && (numberOfFormulae < 2 || softmenu[softmenuStack[0].softmenuId].menuItem != -MNU_EQN)) {
+        else if((calcMode == CM_NORMAL || calcMode == CM_AIM || calcMode == CM_NIM) && (numberOfFormulae < 2 || softmenu[softmenuStack[0].softmenuId].menuItem != -MNU_EQN) && (calcMode != CM_AIM || alphaCase == AC_UPPER)) {
           if(calcMode == CM_NIM) closeNim();
           if(calcMode == CM_AIM) closeAim();
           fnBst(NOPARAM);
@@ -2092,7 +2092,7 @@ void fnKeyDown(uint16_t unusedButMandatoryParameter) {
         if(currentSoftmenuScrolls()) {
           menuDown();
         }
-        else if((calcMode == CM_NORMAL || calcMode == CM_AIM || calcMode == CM_NIM) && (numberOfFormulae < 2 || softmenu[softmenuStack[0].softmenuId].menuItem != -MNU_EQN)) {
+        else if((calcMode == CM_NORMAL || calcMode == CM_AIM || calcMode == CM_NIM) && (numberOfFormulae < 2 || softmenu[softmenuStack[0].softmenuId].menuItem != -MNU_EQN) && (calcMode != CM_AIM || alphaCase == AC_LOWER)) {
           if(calcMode == CM_NIM) closeNim();
           if(calcMode == CM_AIM) closeAim();
           fnSst(NOPARAM);
