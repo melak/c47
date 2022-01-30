b0c005c932ca0ad0fe60735985badb9bd5c04a36
MihailJP <mihailjp@gmail.com>
2022-01-30 11:33:40 +0900

    Revert "WIP AGRAPH"
    This reverts commit ce1c2d9d30ded4adac8717105f8afdf87d795f58.

--- src/wp43s/screen.h
+++ src/wp43s/screen.h
@@ -33,7 +33,6 @@ void       fnScreenDump                       (uint16_t unusedButMandatoryParame
 void       fnClLcd                            (uint16_t unusedButMandatoryParameter);
 void       fnPixel                            (uint16_t unusedButMandatoryParameter);
 void       fnPoint                            (uint16_t unusedButMandatoryParameter);
-void       fnAGraph                           (uint16_t regist);
 
 #ifdef PC_BUILD
   /**
