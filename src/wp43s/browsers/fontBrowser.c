af59ded3d81b1ca0db895a26ed4ecac0b6c25775
Jaco Mostert <jaco@cocoon-creations.com>
2022-01-30 23:49:01 +0200

    Cut fontbrowser to fit in QSPI

--- src/wp43s/browsers/fontBrowser.c
+++ src/wp43s/browsers/fontBrowser.c
@@ -40,7 +40,7 @@
    * \return void
    ***********************************************/
   void initFontBrowser(void) {
-  #ifndef SAVESPACE_JM
+  #ifndef SAVESPACE_JM_FB
     uint16_t g;
 
     numLinesNumericFont  = 0;
@@ -79,7 +79,7 @@
         exit(-1);
       }
     #endif // PC_BUILD
-  #endif //SAVESPACE_JM
+  #endif //SAVESPACE_JM_FB
   }
 
 
@@ -91,7 +91,7 @@
    * \return void
    ***********************************************/
   void fontBrowser(uint16_t unusedButMandatoryParameter) {
-  #ifndef SAVESPACE_JM
+  #ifndef SAVESPACE_JM_FB
     uint16_t x, y, first;
 
     hourGlassIconEnabled = false;
@@ -161,6 +161,6 @@
     else {
       displayBugScreen("In function showFonts: This should never happen!");
     }
-  #endif //SAVESPACE_JM
+  #endif //SAVESPACE_JM_FB
   }
 #endif // TESTSUITE_BUILD
--- src/wp43s/defines.h
+++ src/wp43s/defines.h
@@ -21,10 +21,12 @@
 #define DEFINES_H
 
 #ifdef DMCP_BUILD
-  #undef SAVESPACE_JM //To remove some code to make fit without QSPI: Less fontbrowser, less registerbrowser
+  #undef  SAVESPACE_JM       //To remove some code to make fit without QSPI: Less flagbrowser, less registerbrowser
+  #define SAVESPACE_JM_FB    //To remove some code to make fit without QSPI: Less fontbrowser
   #define SAVE_SPACE_DM42_12 //To remove math functions Zeta, Slvq, ortho_poly, elliptic, beta, bessel
 #else
   #undef SAVESPACE_JM
+  #undef SAVESPACE_JM_FB
   #undef SAVE_SPACE_DM42_12
 #endif
 
