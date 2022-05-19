4101f3c83b353ec1f088cd943aa464994234113e
MihailJP <mihailjp@gmail.com>
2022-05-19 22:25:59 +0900

    Fix issue around up and down keys for multi-view menu

--- src/wp43s/defines.h
+++ src/wp43s/defines.h
@@ -685,10 +685,16 @@ typedef enum {
 #define LM_REGISTERS_PARTIAL                       6
 
 // Screen updating mode
-#define SCRUPD_AUTO                                0
-#define SCRUPD_MANUAL_STATUSBAR                    1
-#define SCRUPD_MANUAL_STACK                        2
-#define SCRUPD_MANUAL_MENU                         4
+#define SCRUPD_AUTO                             0x00
+#define SCRUPD_MANUAL_STATUSBAR                 0x01
+#define SCRUPD_MANUAL_STACK                     0x02
+#define SCRUPD_MANUAL_MENU                      0x04
+//#define SCRUPD_RESERVED_3                       0x08
+//#define SCRUPD_SKIP_STATUSBAR_ONE_TIME          0x10
+#define SCRUPD_SKIP_STACK_ONE_TIME              0x20
+//#define SCRUPD_SKIP_MENU_ONE_TIME               0x40
+//#define SCRUPD_RESERVED_7                       0x80
+#define SCRUPD_ONE_TIME_FLAGS                   0xf0
 
 // Statistical sums TODO: optimize size of SIGMA_N, _X, _Y, _XMIN, _XMAX, _YMIN, and _YMAX. Thus, saving 2×(7×60 - 4 - 6×16) = 640 bytes
 #define SUM_X                                      1
