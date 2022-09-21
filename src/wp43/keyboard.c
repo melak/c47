640d81977a8c0c397e02d81f4473cee3a40c5728
MihailJP <mihailjp@gmail.com>
2022-09-21 19:18:35 +0900

    Up/down keys no longer close AIM

--- src/wp43/keyboard.c
+++ src/wp43/keyboard.c
@@ -2449,13 +2449,10 @@ void fnKeyUp(uint16_t unusedButMandatoryParameter) {
         if(currentSoftmenuScrolls()) {
           menuUp();
         }
-        else if((calcMode == CM_NORMAL || calcMode == CM_AIM || calcMode == CM_NIM) && (numberOfFormulae < 2 || softmenu[softmenuStack[0].softmenuId].menuItem != -MNU_EQN) && (calcMode != CM_AIM || alphaCase == AC_UPPER)) {
+        else if((calcMode == CM_NORMAL || calcMode == CM_NIM) && (numberOfFormulae < 2 || softmenu[softmenuStack[0].softmenuId].menuItem != -MNU_EQN)) {
           screenUpdatingMode = SCRUPD_AUTO;
           if(calcMode == CM_NIM) {
             closeNim();
-          }
-          if(calcMode == CM_AIM) {
-            closeAim();
           }
           fnBst(NOPARAM);
           #if defined(DMCP_BUILD)
@@ -2522,10 +2519,7 @@ void fnKeyUp(uint16_t unusedButMandatoryParameter) {
         else if(currentSoftmenuScrolls()) {
           menuUp();
         }
-        else {
-          if(getSystemFlag(FLAG_ALPHA) && aimBuffer[0] == 0 && !tam.mode) {
-            pemAlpha(ITM_BACKSPACE);
-          }
+        else if(!getSystemFlag(FLAG_ALPHA)) {
           fnBst(NOPARAM);
         }
         break;
@@ -2623,13 +2617,10 @@ void fnKeyDown(uint16_t unusedButMandatoryParameter) {
         if(currentSoftmenuScrolls()) {
           menuDown();
         }
-        else if((calcMode == CM_NORMAL || calcMode == CM_AIM || calcMode == CM_NIM) && (numberOfFormulae < 2 || softmenu[softmenuStack[0].softmenuId].menuItem != -MNU_EQN) && (calcMode != CM_AIM || alphaCase == AC_LOWER)) {
+        else if((calcMode == CM_NORMAL || calcMode == CM_NIM) && (numberOfFormulae < 2 || softmenu[softmenuStack[0].softmenuId].menuItem != -MNU_EQN)) {
           screenUpdatingMode = SCRUPD_AUTO;
           if(calcMode == CM_NIM) {
             closeNim();
-          }
-          if(calcMode == CM_AIM) {
-            closeAim();
           }
           fnSst(NOPARAM);
         }
@@ -2691,11 +2682,7 @@ void fnKeyDown(uint16_t unusedButMandatoryParameter) {
         else if(currentSoftmenuScrolls()) {
           menuDown();
         }
-        else {
-          if(getSystemFlag(FLAG_ALPHA) && aimBuffer[0] == 0 && !tam.mode) {
-            pemAlpha(ITM_BACKSPACE);
-            fnBst(NOPARAM); // Set the PGM pointer to the original position
-          }
+        else if(!getSystemFlag(FLAG_ALPHA)) {
           fnSst(NOPARAM);
         }
         break;
