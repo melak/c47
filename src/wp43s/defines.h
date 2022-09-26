96ad42844009ccc4fcef512e94b503d1cbaed395
jaymos <jaco@cocoon-creations.com>
2022-09-26 22:27:15 +0200

    Edited compile options for single file

--- src/wp43s/defines.h
+++ src/wp43s/defines.h
@@ -70,9 +70,9 @@
   #ifndef TWO_FILE_PGM //---------THESE ARE THE EXCLUSIONS TO MAKE IT FIT WHILE NOT USING QSPI
     #define SAVE_SPACE_DM42    //013968 bytes: KEYS (USER_V43LT, USER_V43, USER_C43, USER_43S); STAT DEMOS 0,1,2; 
     #define SAVE_SPACE_DM42_0  //001032 bytes: Startup test values in registers; 
-  #define SAVE_SPACE_DM42_1  //001568 bytes: STAT DEMOS 105-107-109
-  //#define SAVE_SPACE_DM42_2  //005672 bytes: XEQM
-  //#define SAVE_SPACE_DM42_6  //001648 bytes: ELEC functions
+    #define SAVE_SPACE_DM42_1  //001568 bytes: STAT DEMOS 105-107-109
+    #define SAVE_SPACE_DM42_2  //005672 bytes: XEQM
+    #define SAVE_SPACE_DM42_6  //001648 bytes: ELEC functions
   //#define SAVE_SPACE_DM42_7  //002144 bytes: KEYS USER_DM42; USER_SHIFTS;
   //#define SAVE_SPACE_DM42_8  //007136 bytes: Standard Flag-, Register-, Font- Browser functions
   //#define SAVE_SPACE_DM42_9  //004448 bytes: SHOW (new C43)
@@ -92,11 +92,7 @@
 //    #define SAVE_SPACE_DM42_13GRF_JM //           JM graphics
     #define SAVE_SPACE_DM42_15    //           without all distributions, i.e. binomial, cauchy, chi
   #endif
-
 #endif
-
-//removed option code
-//    #define SAVE_SPACE_DM42_4  //000736 bytes: XY GRAPHDEMOS (Plot)
 
 
 
