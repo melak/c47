bdc763494ac35baa903250bc460d0db889872aab
jaymos <jaco@cocoon-creations.com>
2022-10-01 16:33:15 +0200

    Restores DEG RAD GRAD

--- src/wp43/softmenus.c
+++ src/wp43/softmenus.c
@@ -172,9 +172,9 @@ TO_QSPI const int16_t menu_MODE[]        = { ITM_DRG,                       ITM_
                                              ITM_SYS,                       -MNU_TRI,                   ITM_NULL,                 ITM_NULL,              ITM_RECT,                    ITM_POLAR,                         //JM
 */
 
-TO_QSPI const int16_t menu_MODE[]        = { ITM_DRG,                       ITM_DEG2,                   ITM_RAD2,                 ITM_GRAD2,             ITM_DMS2,                    ITM_CFG,                       
-                                             ITM_SYS,                       ITM_DEG,                    ITM_RAD,                  ITM_GRAD,              ITM_DMS,                     -MNU_TRI,                         //JM
-                                             ITM_NULL,                       ITM_NULL,                   ITM_NULL,                 ITM_NULL,              ITM_RECT,                    ITM_POLAR,                         //JM
+TO_QSPI const int16_t menu_MODE[]        = { ITM_DRG,                       ITM_DEG,                    ITM_RAD,                  ITM_GRAD,              ITM_DMS,                     ITM_CFG,                       
+                                             ITM_SYS,                       ITM_DEG2,                   ITM_RAD2,                 ITM_GRAD2,             ITM_DMS2,                    -MNU_TRI,                         //JM
+                                             ITM_NULL,                      ITM_NULL,                   ITM_NULL,                 ITM_NULL,              ITM_RECT,                    ITM_POLAR,                         //JM
 
                                              ITM_SSIZE4,                    ITM_SSIZE8,                 ITM_CB_CPXRES,            ITM_CB_SPCRES,         ITM_DENMAX2,                 ITM_CFG,                           //JM
                                              ITM_ERPN,                      ITM_SETSIG2,                ITM_RMODE,                ITM_EXFRAC,            ITM_DENANY,                  ITM_DENFIX,                          //JM
