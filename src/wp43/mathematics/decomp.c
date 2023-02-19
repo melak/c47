337d33213efa909af664d6517d5e9b3723136234
MihailJP <mihailjp@gmail.com>
2023-02-19 18:46:41 +0900

    Fix inconsistency of DECOMP related to infinite parameter
    DECOMP now always returns long integers
    DECOMP now returns 0, 0 if the argument is NaN

--- src/wp43/mathematics/decomp.c
+++ src/wp43/mathematics/decomp.c
@@ -90,12 +90,12 @@ void decompReal(void) {
   liftStack();
 
   if(real34IsNaN(&x)) {
-    convertRealToReal34ResultRegister(const_NaN, REGISTER_X); // Denominator = NaN
-    convertRealToReal34ResultRegister(const_NaN, REGISTER_Y); // Numerator = NaN
+    convertReal34ToLongIntegerRegister(const34_0, REGISTER_X, DEC_ROUND_HALF_DOWN); // Denominator = 0
+    convertReal34ToLongIntegerRegister(const34_0, REGISTER_Y, DEC_ROUND_HALF_DOWN); // Numerator = 0
   }
   else if(real34IsInfinite(&x)) {
-    convertRealToReal34ResultRegister(const_0, REGISTER_X); // Denominator = 0
-    realToReal34(real34IsNegative(REGISTER_REAL34_DATA(REGISTER_X)) ? const__1 : const_1, REGISTER_REAL34_DATA(REGISTER_Y)); // Numerator = +/- 1
+    convertReal34ToLongIntegerRegister(const34_0, REGISTER_X, DEC_ROUND_HALF_DOWN); // Denominator = 0
+    convertReal34ToLongIntegerRegister(real34IsNegative(&x) ? const34__1 : const34_1, REGISTER_Y, DEC_ROUND_HALF_DOWN); // Numerator = +/- 1
   }
   else {
     uint32_t savedDenMax = denMax;
