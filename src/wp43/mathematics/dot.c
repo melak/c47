Makwa60/wp43s

Add test cases for real matrices

src/wp43/mathematics/dot.c
@@ -196,25 +196,23 @@ void dotCplxShoI(void) {
 
 
 void dotRemaRema(void) {
-  #if !defined(TESTSUITE_BUILD)
-    real34Matrix_t y, x;
-    real34_t res;
+  real34Matrix_t y, x;
+  real34_t res;
 
-    linkToRealMatrixRegister(REGISTER_Y, &y);
-    linkToRealMatrixRegister(REGISTER_X, &x);
+  linkToRealMatrixRegister(REGISTER_Y, &y);
+  linkToRealMatrixRegister(REGISTER_X, &x);
 
-    if((realVectorSize(&y) == 0) || (realVectorSize(&x) == 0) || (realVectorSize(&y) != realVectorSize(&x))) {
-      displayCalcErrorMessage(ERROR_MATRIX_MISMATCH, ERR_REGISTER_LINE, REGISTER_X);
-      errorMoreInfo("numbers of elements of %d" STD_CROSS "%d-matrix to %d" STD_CROSS "%d-matrix mismatch",
-          x.header.matrixRows, x.header.matrixColumns,
-          y.header.matrixRows, y.header.matrixColumns);
-    }
-    else {
-      dotRealVectors(&y, &x, &res);
-      reallocateRegister(REGISTER_X, dtReal34, REAL34_SIZE_IN_BYTES, amNone);
-      real34Copy(&res, REGISTER_REAL34_DATA(REGISTER_X));
-    }
-  #endif // !TESTSUITE_BUILD
+  if((realVectorSize(&y) == 0) || (realVectorSize(&x) == 0) || (realVectorSize(&y) != realVectorSize(&x))) {
+    displayCalcErrorMessage(ERROR_MATRIX_MISMATCH, ERR_REGISTER_LINE, REGISTER_X);
+    errorMoreInfo("numbers of elements of %d" STD_CROSS "%d-matrix to %d" STD_CROSS "%d-matrix mismatch",
+        x.header.matrixRows, x.header.matrixColumns,
+        y.header.matrixRows, y.header.matrixColumns);
+  }
+  else {
+    dotRealVectors(&y, &x, &res);
+    reallocateRegister(REGISTER_X, dtReal34, REAL34_SIZE_IN_BYTES, amNone);
+    real34Copy(&res, REGISTER_REAL34_DATA(REGISTER_X));
+  }
 }
 
 

melak/c47

src/wp43/mathematics/dot.c
@@ -287,28 +287,26 @@ void dotCplxShoI(void) {
  * \return void
  ***********************************************/
 void dotRemaRema(void) {
-  #if !defined(TESTSUITE_BUILD)
-    real34Matrix_t y, x;
-    real34_t res;
+  real34Matrix_t y, x;
+  real34_t res;
 
-    linkToRealMatrixRegister(REGISTER_Y, &y);
-    linkToRealMatrixRegister(REGISTER_X, &x);
+  linkToRealMatrixRegister(REGISTER_Y, &y);
+  linkToRealMatrixRegister(REGISTER_X, &x);
 
-    if((realVectorSize(&y) == 0) || (realVectorSize(&x) == 0) || (realVectorSize(&y) != realVectorSize(&x))) {
-      displayCalcErrorMessage(ERROR_MATRIX_MISMATCH, ERR_REGISTER_LINE, REGISTER_X);
-      #if (EXTRA_INFO_ON_CALC_ERROR == 1)
-        sprintf(errorMessage, "numbers of elements of %d" STD_CROSS "%d-matrix to %d" STD_CROSS "%d-matrix mismatch",
-                x.header.matrixRows, x.header.matrixColumns,
-                y.header.matrixRows, y.header.matrixColumns);
-        moreInfoOnError("In function dotRemaRema:", errorMessage, NULL, NULL);
-      #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-    }
-    else {
-      dotRealVectors(&y, &x, &res);
-      reallocateRegister(REGISTER_X, dtReal34, REAL34_SIZE, amNone);
-      real34Copy(&res, REGISTER_REAL34_DATA(REGISTER_X));
-    }
-  #endif // !TESTSUITE_BUILD
+  if((realVectorSize(&y) == 0) || (realVectorSize(&x) == 0) || (realVectorSize(&y) != realVectorSize(&x))) {
+    displayCalcErrorMessage(ERROR_MATRIX_MISMATCH, ERR_REGISTER_LINE, REGISTER_X);
+    #if (EXTRA_INFO_ON_CALC_ERROR == 1)
+      sprintf(errorMessage, "numbers of elements of %d" STD_CROSS "%d-matrix to %d" STD_CROSS "%d-matrix mismatch",
+              x.header.matrixRows, x.header.matrixColumns,
+              y.header.matrixRows, y.header.matrixColumns);
+      moreInfoOnError("In function dotRemaRema:", errorMessage, NULL, NULL);
+    #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
+  }
+  else {
+    dotRealVectors(&y, &x, &res);
+    reallocateRegister(REGISTER_X, dtReal34, REAL34_SIZE, amNone);
+    real34Copy(&res, REGISTER_REAL34_DATA(REGISTER_X));
+  }
 }
 
 /********************************************//**