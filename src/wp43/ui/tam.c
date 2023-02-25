Makwa60/wp43s

Implement DELITM for programs

src/wp43/ui/tam.c
@@ -114,6 +114,18 @@ void tamReset(void) {
         }
       }
 
+      case ITM_DELITM: {
+        switch(-softmenu[softmenuStack[0].softmenuId].menuItem) {
+          case MNU_RAM:
+          case MNU_FLASH: {
+            return ITM_DELITM_PROG;
+          }
+          default: {
+            return ITM_DELITM;
+          }
+        }
+      }
+
       default: {
         return tam.function;
       }
@@ -782,7 +794,7 @@ void tamReset(void) {
           }
         }
       }
-      else if(tam.mode == tmLabel || tam.mode == tmSolve || (tam.mode == tmKey && tam.keyInputFinished)) {
+      else if(tam.mode == tmLabel || tam.mode == tmSolve || (tam.mode == tmKey && tam.keyInputFinished) || (tam.mode == tmDelItem && (softmenu[softmenuStack[0].softmenuId].menuItem == -MNU_RAM || softmenu[softmenuStack[0].softmenuId].menuItem == -MNU_FLASH))) {
         value = findNamedLabel(buffer);
         if(value == INVALID_VARIABLE && tam.function != ITM_LBL && tam.function != ITM_LBLQ) {
           if(calcMode != cmPem && getSystemFlag(FLAG_IGN1ER)) {

melak/c47

src/wp43/ui/tam.c
@@ -118,6 +118,18 @@
         }
       }
 
+      case ITM_DELITM: {
+        switch(-softmenu[softmenuStack[0].softmenuId].menuItem) {
+          case MNU_RAM:
+          case MNU_FLASH: {
+            return ITM_DELITM_PROG;
+          }
+          default: {
+            return ITM_DELITM;
+          }
+        }
+      }
+
       default: {
         return tam.function;
       }
@@ -804,7 +816,7 @@
           }
         }
       }
-      else if(tam.mode == TM_LABEL || tam.mode == TM_SOLVE || (tam.mode == TM_KEY && tam.keyInputFinished)) {
+      else if(tam.mode == TM_LABEL || tam.mode == TM_SOLVE || (tam.mode == TM_KEY && tam.keyInputFinished) || (tam.mode == TM_DELITM && (softmenu[softmenuStack[0].softmenuId].menuItem == -MNU_RAM || softmenu[softmenuStack[0].softmenuId].menuItem == -MNU_FLASH))) {
         value = findNamedLabel(buffer);
         if(value == INVALID_VARIABLE && tam.function != ITM_LBL && tam.function != ITM_LBLQ) {
           displayCalcErrorMessage(ERROR_LABEL_NOT_FOUND, ERR_REGISTER_LINE, REGISTER_X);