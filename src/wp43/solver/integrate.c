melak/c47

commit 9b4e1abdee4c4854d20a2681f754928a3889f1aa

Fix crash issue of SOLVE not preceded by PGMSLV in PGM

src/wp43/solver/integrate.c
@@ -115,6 +115,14 @@ void fnIntegrate(uint16_t labelOrVariable) {
       }
     }
   }
+  else if(labelOrVariable >= FIRST_NAMED_VARIABLE && labelOrVariable <= LAST_NAMED_VARIABLE && currentSolverProgram >= numberOfLabels) {
+    displayCalcErrorMessage(ERROR_NO_PROGRAM_SPECIFIED, ERR_REGISTER_LINE, REGISTER_X);
+    #if (EXTRA_INFO_ON_CALC_ERROR == 1)
+      sprintf(errorMessage, "label %u not found", labelOrVariable);
+      moreInfoOnError("In function fnIntegrate:", errorMessage, NULL, NULL);
+    #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
+    adjustResult(REGISTER_X, false, false, REGISTER_X, -1, -1);
+  }
   else if(labelOrVariable >= FIRST_NAMED_VARIABLE && labelOrVariable <= LAST_NAMED_VARIABLE) {
     real_t acc, ulim, llim, res;
     real34ToReal(REGISTER_REAL34_DATA(RESERVED_VARIABLE_ACC),  &acc);

src/wp43/solver/solve.c
@@ -116,6 +116,14 @@ void fnSolve(uint16_t labelOrVariable) {
     }
     adjustResult(REGISTER_X, false, false, REGISTER_X, -1, -1);
   }
+  else if(labelOrVariable >= FIRST_NAMED_VARIABLE && labelOrVariable <= LAST_NAMED_VARIABLE && currentSolverProgram >= numberOfLabels) {
+    displayCalcErrorMessage(ERROR_NO_PROGRAM_SPECIFIED, ERR_REGISTER_LINE, REGISTER_X);
+    #if (EXTRA_INFO_ON_CALC_ERROR == 1)
+      sprintf(errorMessage, "label %u not found", labelOrVariable);
+      moreInfoOnError("In function fnSolve:", errorMessage, NULL, NULL);
+    #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
+    adjustResult(REGISTER_X, false, false, REGISTER_X, -1, -1);
+  }
   else if(labelOrVariable >= FIRST_NAMED_VARIABLE && labelOrVariable <= LAST_NAMED_VARIABLE) {
     // Execute
     real34_t z, y, x;
@@ -210,7 +218,7 @@ void fnSolve(uint16_t labelOrVariable) {
     displayCalcErrorMessage(ERROR_OUT_OF_RANGE, ERR_REGISTER_LINE, REGISTER_X);
     #if (EXTRA_INFO_ON_CALC_ERROR == 1)
       sprintf(errorMessage, "unexpected parameter %u", labelOrVariable);
-      moreInfoOnError("In function fnPgmSlv:", errorMessage, NULL, NULL);
+      moreInfoOnError("In function fnSolve:", errorMessage, NULL, NULL);
     #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
     adjustResult(REGISTER_X, false, false, REGISTER_X, -1, -1);
   }

src/wp43/config.c
@@ -1142,6 +1142,11 @@ void doFnReset(uint16_t confirmation, bool_t autoSav) {
     numberOfFormulae = 0;
     currentFormula = 0;
 
+    currentSolverStatus = 0;
+    currentSolverProgram = 0xffffu;
+    currentSolverVariable = INVALID_VARIABLE;
+    currentSolverNestingDepth = 0;
+
     graphVariable = 0;
 
     // Timer application

src/wp43/defines.h
@@ -336,8 +336,9 @@
 #define ERROR_NO_GLOBAL_LABEL                     51
 #define ERROR_INVALID_DATA_TYPE_FOR_POLAR_RECT    52
 #define ERROR_BAD_INPUT                           53 // This error is not in ReM and cannot occur (theoretically).
+#define ERROR_NO_PROGRAM_SPECIFIED                54
 
-#define NUMBER_OF_ERROR_CODES                     54
+#define NUMBER_OF_ERROR_CODES                     55
 #define SIZE_OF_EACH_ERROR_MESSAGE                48
 
 #define NUMBER_OF_BUG_SCREEN_MESSAGES             10

src/wp43/error.c
@@ -103,6 +103,7 @@ TO_QSPI const char errorMessages[NUMBER_OF_ERROR_CODES][SIZE_OF_EACH_ERROR_MESSA
 /* 51 */  "No global label in this program",
 /* 52 */  "Invalid input data type for polar/rect mode",
 /* 53 */  "Bad input", // This error is not in ReM and cannot occur (theoretically).
+/* 54 */  "No program specified",
 };
 
 

commit 19162d6c72b31a02e5ef677e7af4b2c75df3ece8

Fix EQN
It was broken by 9b4e1abd

src/wp43/solver/integrate.c
@@ -115,7 +115,7 @@ void fnIntegrate(uint16_t labelOrVariable) {
       }
     }
   }
-  else if(labelOrVariable >= FIRST_NAMED_VARIABLE && labelOrVariable <= LAST_NAMED_VARIABLE && currentSolverProgram >= numberOfLabels) {
+  else if(!(currentSolverStatus & SOLVER_STATUS_USES_FORMULA) && labelOrVariable >= FIRST_NAMED_VARIABLE && labelOrVariable <= LAST_NAMED_VARIABLE && currentSolverProgram >= numberOfLabels) {
     displayCalcErrorMessage(ERROR_NO_PROGRAM_SPECIFIED, ERR_REGISTER_LINE, REGISTER_X);
     #if(EXTRA_INFO_ON_CALC_ERROR == 1)
       sprintf(errorMessage, "label %u not found", labelOrVariable);

src/wp43/solver/solve.c
@@ -116,7 +116,7 @@ void fnSolve(uint16_t labelOrVariable) {
     }
     adjustResult(REGISTER_X, false, false, REGISTER_X, -1, -1);
   }
-  else if(labelOrVariable >= FIRST_NAMED_VARIABLE && labelOrVariable <= LAST_NAMED_VARIABLE && currentSolverProgram >= numberOfLabels) {
+  else if(!(currentSolverStatus & SOLVER_STATUS_USES_FORMULA) && labelOrVariable >= FIRST_NAMED_VARIABLE && labelOrVariable <= LAST_NAMED_VARIABLE && currentSolverProgram >= numberOfLabels) {
     displayCalcErrorMessage(ERROR_NO_PROGRAM_SPECIFIED, ERR_REGISTER_LINE, REGISTER_X);
     #if(EXTRA_INFO_ON_CALC_ERROR == 1)
       sprintf(errorMessage, "label %u not found", labelOrVariable);