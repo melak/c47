0dca0a899164a7112ba70a0eb73aa7c3123ee25e
jaymos <jaco@cocoon-creations.com>
2023-07-16 23:49:58 +0200

    Fix-forum-bugs-relating-to-RCL,STO,XEQ,-preview-content-and-screen-pixel-positions

--- src/wp43/keyboard.c
+++ src/wp43/keyboard.c
@@ -1570,7 +1570,7 @@ bool_t nimWhenButtonPressed = false;                  //PHM eRPN 2021-07
         #if defined(PC_BUILD_TELLTALE)
           sprintf(tmp,"keyboard.c: btnPressed 2--> processKeyAction(%d) which is str:%s\n",item,(char *)data); jm_show_calc_state(tmp);
         #endif //PC_BUILD_TELLTALE
-
+        //printf("----- %i ----- |%s|\n",item,funcParam);
         if(!keyActionProcessed) {
 
           showFunctionName(item, 1000, funcParam);// "SF:B"); // 1000ms = 1s
