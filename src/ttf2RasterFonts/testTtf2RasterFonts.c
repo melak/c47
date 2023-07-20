9042e29e9e14601bd2dc477bf32683b84bded83f
Over_score <martin@lorang.net>
2023-07-20 18:03:48 +0200

    Tying to save QSPI bytes

--- src/ttf2RasterFonts/testTtf2RasterFonts.c
+++ src/ttf2RasterFonts/testTtf2RasterFonts.c
@@ -1,17 +1,17 @@
-/* This file is part of C47.
+/* This file is part of 43S.
  *
- * C47 is free software: you can redistribute it and/or modify
+ * 43S is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * C47 is distributed in the hope that it will be useful,
+ * 43S is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with C47.  If not, see <http://www.gnu.org/licenses/>.
+ * along with 43S.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
