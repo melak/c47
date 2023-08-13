c5e3b34c7390fb0673c9f1d56ed4a8c93ce68207
jaymos <jaco@cocoon-creations.com>
2023-08-13 21:57:53 +0200

    version update

--- src/wp43/defines.h
+++ src/wp43/defines.h
@@ -24,9 +24,9 @@
 // JM VARIOUS OPTIONS
 //*********************************
 
-#define VERSION1 "0.108.11.19"     // major release . minor release . tracked build - internal un/tracked subrelease : alpha/beta/rc1
+#define VERSION1 "0.108.12.00"     // major release . minor release . tracked build - internal un/tracked subrelease : alpha/beta/rc1
 
-//2023-07-30-0.108.11.10
+//2023-08-13-0.108.12.00-RC-Snapshot
 
   #undef SAVE_SPACE_DM42
   #undef SAVE_SPACE_DM42_0
