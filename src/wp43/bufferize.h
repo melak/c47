b73adf21d595b2f483d7985090fb781fb41d02a1
jaymos <jaco@cocoon-creations.com>
2023-08-26 16:10:26 +0200

    compiler space

--- src/wp43/bufferize.h
+++ src/wp43/bufferize.h
@@ -10,7 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have receved a copy of the GNU General Public License
+ * You should have received a copy of the GNU General Public License
  * along with 43S.  If not, see <http://www.gnu.org/licenses/>.
  */
 
